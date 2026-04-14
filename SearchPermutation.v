6606ccc9177bdea68165556b244d9ef68e2403bd
Zuhaib2004 <146606034+Zuhaib2004@users.noreply.github.com>
2026-04-14 17:52:16 -0500

    basic functions ,starter code

--- SearchPermutation.v
+++ SearchPermutation.v
@@ -11,7 +11,6 @@ From Stdlib Require Import Arith List Permutation.
 (* ################################################################# *)
 (** * Linear Search *)
 
-(** Decision procedure for list membership, walking left to right. *)
 Fixpoint linear_search (e : nat) (l : list nat) : bool :=
   match l with
   | nil => false
@@ -22,10 +21,7 @@ Lemma linear_search_cons (e a : nat) (xs : list nat) :
   linear_search e (a :: xs) = if Nat.eqb e a then true else linear_search e xs.
 Proof. reflexivity. Qed.
 
-(* ================================================================= *)
-(** ** Correctness with [In] *)
 
-(** [linear_search] agrees with the inductive [In] predicate. *)
 Lemma linear_search_true_iff_In :
   forall (e : nat) (l : list nat), linear_search e l = true <-> In e l.
 Proof.
@@ -43,11 +39,6 @@ Proof.
           [ reflexivity | now apply IH ].
 Qed.
 
-(* ################################################################# *)
-(** * Permutation Invariance *)
-
-(** Main property from the proposal:
-    permuting the list does not change whether [linear_search] finds [e]. *)
 Theorem search_correct_on_permutations :
   forall (l l_perm : list nat) (e : nat),
     Permutation l l_perm ->
@@ -60,20 +51,4 @@ Proof.
   - intro Hin.
     apply Permutation_in with (l := l_perm) (l' := l); [| assumption].
     now apply Permutation_sym.
-Qed.
-
-(* ================================================================= *)
-(** ** Optional Corollary *)
-
-(** Matches secondary goal “member => found after permute”:
-    if [e] is in [l], it is still found in any permutation. *)
-Corollary In_linear_search_on_permutation :
-  forall (l l_perm : list nat) (e : nat),
-    Permutation l l_perm ->
-    In e l ->
-    linear_search e l_perm = true.
-Proof.
-  intros * Hperm Hin.
-  apply linear_search_true_iff_In.
-  now apply Permutation_in with (l := l) (l' := l_perm).
 Qed.
